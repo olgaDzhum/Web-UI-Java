5266f7901822a037ff87fe158fd24cfdf64121b0
djymka <djymka@gmail.com>
2021-12-03 19:18:48 +0300

    Lesson 4

---
+++ src/main/java/ru/geekbrains/lesson4/Main.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double result = new Triangle(4,6,8).calculateTriangleSquare();
+        System.out.println(result);
+                
+    }
+    
+}
